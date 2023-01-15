0da46c56a418b6f6fe08157d9a01f86b078ad67d
chaituvarma-109 <chaitanyavarma.u@gmail.com>
2023-01-16 00:43:34 +0530

    modified post route

--- backend/todo/routers/users.py
+++ backend/todo/routers/users.py
@@ -1,10 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
 from .. import schemas
 from ..database import get_db
 from ..repository import users
-
 
 router = APIRouter(
     prefix='/item',
@@ -12,7 +11,8 @@ router = APIRouter(
 )
 
 
-@router.post('/', response_model=schemas.ShowUser)
+# response_model=schemas.ShowUser
+@router.post('/', status_code=status.HTTP_201_CREATED)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     return users.create_user(request, db)
 
